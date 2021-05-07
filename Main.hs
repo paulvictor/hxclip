d5a2a1e2c1813f654c6f2d9cfb22bda2a2aba1e7
Paul Victor Raj <paulvictor@gmail.com>
2021-05-07 18:30:40 +0530

    Improvements + more robust + cleanups

--- Main.hs
+++ Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Main(main) where
 
 import Data.Maybe (fromMaybe)
 import System.Environment (lookupEnv)
+import qualified Crypto.Gpgme as GPG
+import qualified Data.ByteString.Char8 as BS
 import qualified Graphics.X11.Xlib.Display as X
 import qualified Graphics.X11.Xlib.Atom as X
 import qualified Graphics.X11.Xlib.Window as X
@@ -11,27 +14,41 @@ import qualified Graphics.X11.Xlib.Extras as X
 import qualified Graphics.X11.Xlib.Event as X
 import qualified Graphics.X11.Types as X
 import qualified Graphics.X11.Xlib.Types as X
-import qualified Data.ByteString.Char8 as BS
-import qualified Crypto.Gpgme as GPG
+import qualified Libnotify.C.Notify as LN
+import qualified Libnotify.C.NotifyNotification as LN
 import Data.IORef (IORef, newIORef, writeIORef, readIORef)
 import Foreign.C.String (castCCharToChar, castCharToCChar)
+import System.IO (hPutStr, stderr)
 import System.IO.Unsafe (unsafePerformIO)
-import Control.Monad (forever)
+import Control.Monad (forever, when)
 import Data.ByteString (ByteString)
-import System.Posix.Signals (installHandler, sigTERM, sigINT, Handler(CatchOnce))
-import System.Exit (exitSuccess)
 import Data.Functor (void, (<&>))
 import Options.Applicative
+
+data ClipBoard
+  = ClipBoard
+  { contents :: ByteString
+  , storedEncrypted :: Bool }
 
 display :: X.Display
 display = unsafePerformIO $
   lookupEnv "DISPLAY" >>= maybe (error "$DISPLAY is not set") X.openDisplay
+
 clipboard :: X.Atom
 clipboard = unsafePerformIO $ X.internAtom display "CLIPBOARD" False
+
+passwordClipboard :: X.Atom
+passwordClipboard = unsafePerformIO $ X.internAtom display "SECONDARY" False
+
 incr :: X.Atom
 incr = unsafePerformIO $ X.internAtom display "INCR" False
+
+passwordString :: X.Atom
+passwordString = unsafePerformIO $ X.internAtom display "UTF8_STRING" False
+
 utf8String :: X.Atom
 utf8String = unsafePerformIO $ X.internAtom display "UTF8_STRING" False
+
 hxclip :: X.Atom
 hxclip = unsafePerformIO $ X.internAtom display "HXCLIP" False
 
@@ -42,9 +59,11 @@ runInDefaultCtx f = do
        fromMaybe (error "GNUPGHOME not set")
   GPG.withCtx gpghomedir "C" GPG.OpenPGP f
 
+shouldUseLibNotificationRef :: IORef Bool
+shouldUseLibNotificationRef = unsafePerformIO $ newIORef True
+
 main :: IO ()
 main = do
-  installSignalHandlers
   pubKey <-
      getPubKeyFromCLIArgs <&>
        fromMaybe (error "Unable to initialize key with specified fingerprint")
@@ -52,15 +71,16 @@ main = do
     X.rootWindow display (X.defaultScreen display)
   window <-
     X.createSimpleWindow display rootWindow (-10) (-10) 1 1 0 0 0
-  -- clipBoardContents :: IORef (Maybe ByteString)
+  -- clipBoardContents :: IORef (Maybe ClipBoard)
   clipBoardRef <- newIORef Nothing
-  grabClipBoard window pubKey clipBoardRef
+  LN.notify_init "hxclip" >>= writeIORef shouldUseLibNotificationRef
+  -- This can be lossy initially since we maintain both in a single buffer.
+  -- We give preference to the password clipboard
+  grabClipBoard window pubKey clipboard clipBoardRef
+  grabClipBoard window pubKey passwordClipboard clipBoardRef
   forever $
-    serveClipBoard window pubKey clipBoardRef
+    manageClipBoard window pubKey clipBoardRef
   where
-  installSignalHandlers = do
-    void $ installHandler sigINT (CatchOnce exitSuccess) Nothing
-    void $ installHandler sigTERM (CatchOnce exitSuccess) Nothing
   getPubKeyFromCLIArgs = do
     fingerprint <-
       execParser $ info optionsParser fullDesc
@@ -69,57 +89,60 @@ main = do
   optionsParser =
     BS.pack <$> (strOption $ long "gpg-fingerprint")
 
-serveClipBoard :: X.Window -> GPG.Key -> IORef (Maybe ByteString) -> IO ()
-serveClipBoard window gPGKey clipBoardRef = do
+manageClipBoard :: X.Window -> GPG.Key -> IORef (Maybe ClipBoard) -> IO ()
+manageClipBoard window gPGKey clipBoardRef = do
   (eventType, event) <-
     X.allocaXEvent (\xevPtr -> do
       X.nextEvent display xevPtr
       (,) <$> X.get_EventType xevPtr <*> X.getEvent xevPtr)
   if
-    | eventType == X.selectionClear -> do
-     output $ "Lost control of clipboard. Regaining now"
-     grabClipBoard window gPGKey clipBoardRef
+    | eventType == X.selectionClear ->
+      -- Some other application copied data into the selection. Take over!!
+      case event of
+        (X.SelectionClear _ _ _ _ evWindow evSelection _) -> do
+          windowName <-
+            maybe "" ("from " <>) <$> X.fetchName display evWindow
+          (X.getAtomName display evSelection) >>= maybe
+            (output $ "Unable to get clipboard name")
+            (\name -> output $ "Lost control of " <> name <> windowName <> ". Regaining now..")
+          grabClipBoard window gPGKey evSelection clipBoardRef
+        _ -> output $ "Unexpected event : " <> show event
     | eventType == X.selectionRequest ->
        case event of
          (X.SelectionRequest _ _ _ _ _ evRequestor evSelection evTarget evProp _) ->
-           readIORef clipBoardRef >>=
-             maybe
+           if evSelection == passwordClipboard
+           then do
+             output $ "Not serving the password clipboard directly. Use the clipboard"
+             deny evRequestor evSelection evTarget
+           else
+             -- Else doesn't work if managing more than one 2 clipboards
+             readIORef clipBoardRef >>= maybe
                (deny evRequestor evSelection evTarget)
-               (\bs ->
+               (\cboard ->
                  if evTarget == utf8String && evProp /= X.none
-                 then do
-                   runInDefaultCtx $ \ctx ->
-                     GPG.decrypt ctx bs >>=
+                 then
+                   if storedEncrypted cboard
+                   then runInDefaultCtx $ \ctx ->
+                     GPG.decrypt ctx (contents cboard) >>=
                        either
                        (\err -> output $ "Decryption failed : " <> show err)
                        (fulfill evRequestor evSelection evTarget evProp)
+                   else fulfill evRequestor evSelection evTarget evProp (contents cboard)
                  else (deny evRequestor evSelection evTarget))
          _ -> output $ "Unexpected event : " <> show event
-    | otherwise -> serveClipBoard window gPGKey clipBoardRef
+    | otherwise -> manageClipBoard window gPGKey clipBoardRef
 
-deny :: X.Window -> X.Atom -> X.Atom -> IO ()
-deny requestor selection target =
-  X.allocaXEvent (\xEvPtr -> do
-    X.setSelectionNotify xEvPtr requestor selection target X.none X.currentTime
-    X.sendEvent display requestor True X.noEventMask xEvPtr)
-
-fulfill :: X.Window -> X.Atom -> X.Atom -> X.Atom -> ByteString -> IO ()
-fulfill requestor selection target prop contents =
-  X.allocaXEvent (\xEvPtr -> do
-    X.changeProperty8 display requestor prop utf8String X.propModeReplace $
-      castCharToCChar <$> BS.unpack contents
-    X.setSelectionNotify xEvPtr requestor selection target prop X.currentTime
-    X.sendEvent display requestor True X.noEventMask xEvPtr)
-
-grabClipBoard :: X.Window -> GPG.Key -> IORef (Maybe ByteString) -> IO ()
-grabClipBoard window gPGKey clipboardRef = do
-  owner <- X.xGetSelectionOwner display clipboard
+grabClipBoard :: X.Window -> GPG.Key -> X.Atom -> IORef (Maybe ClipBoard) -> IO ()
+grabClipBoard window gPGKey selAtom clipboardRef = do
+  owner <- X.xGetSelectionOwner display selAtom
   if owner == X.none
   then do
-    output "Getting hold of clipboard from no previous owner"
-    X.xSetSelectionOwner display clipboard window X.currentTime
+    (X.getAtomName display selAtom) >>= maybe
+      (output $ "Unable to get clipboard name, still getting hold")
+      (\name -> output $ "Getting hold of " <> name <> " from no previous owner")
+    X.xSetSelectionOwner display selAtom window X.currentTime
   else do
-    X.xConvertSelection display clipboard utf8String hxclip window X.currentTime
+    X.xConvertSelection display selAtom utf8String hxclip window X.currentTime
     serveSelectionNotify
   where
   serveSelectionNotify = do
@@ -138,15 +161,37 @@ grabClipBoard window gPGKey clipboardRef = do
               "Getting ownership of " <> show (length cChars) <> " bytes of data"
             let
               bs = BS.copy $ BS.pack (castCCharToChar <$> cChars)
-            runInDefaultCtx $
-              \ctx ->
-                GPG.encrypt ctx [gPGKey] GPG.NoFlag bs >>=
-                  either
-                    (\err -> output $ "Encryption failed " <> show err)
-                    (\encrypted -> writeIORef clipboardRef $ Just encrypted)
-            X.xSetSelectionOwner display clipboard window X.currentTime)
+            when (not $ BS.null bs) $ -- This check is needed because password managers clear passwords
+              if selAtom == passwordClipboard
+              then runInDefaultCtx $
+                \ctx ->
+                  GPG.encrypt ctx [gPGKey] GPG.NoFlag bs >>=
+                    either
+                      (\err -> output $ "Encryption failed " <> show err)
+                      (\encrypted -> writeIORef clipboardRef $ Just $ ClipBoard encrypted True)
+              else writeIORef clipboardRef $ Just $ ClipBoard bs False
+            X.deleteProperty display window selAtom
+            X.xSetSelectionOwner display selAtom window X.currentTime)
     else serveSelectionNotify
+
+deny :: X.Window -> X.Atom -> X.Atom -> IO ()
+deny requestor selection target =
+  X.allocaXEvent (\xEvPtr -> do
+    X.setSelectionNotify xEvPtr requestor selection target X.none X.currentTime
+    X.sendEvent display requestor True X.noEventMask xEvPtr)
+
+fulfill :: X.Window -> X.Atom -> X.Atom -> X.Atom -> ByteString -> IO ()
+fulfill requestor selection target prop bs =
+  X.allocaXEvent (\xEvPtr -> do
+    X.changeProperty8 display requestor prop utf8String X.propModeReplace $
+      castCharToCChar <$> BS.unpack bs
+    X.setSelectionNotify xEvPtr requestor selection target prop X.currentTime
+    X.sendEvent display requestor True X.noEventMask xEvPtr)
 
 --TODO : Send a desktop notification
 output :: String -> IO ()
-output = putStrLn
+output message = do
+  hPutStr stderr message
+  readIORef shouldUseLibNotificationRef >>= \b ->
+    when b $
+      LN.notify_notification_new "" message "" >>= void . LN.notify_notification_show
